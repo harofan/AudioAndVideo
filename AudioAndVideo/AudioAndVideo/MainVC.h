77125efaf2a31d0b05d22240e24cdf7238f10f8e
fy <fanyang_32012@outlook.com>
2016-09-08 15:36:34 +0800

    导入ijkplayer框架

---
+++ AudioAndVideo/AudioAndVideo/MainVC.h
@@ -0,0 +1,13 @@
+//
+//  MainVC.h
+//  AudioAndVideo
+//
+//  Created by fy on 16/9/8.
+//  Copyright © 2016年 LY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainVC : UIViewController
+
+@end
---
+++ AudioAndVideo/AudioAndVideo/MainVC.m
@@ -0,0 +1,34 @@
+//
+//  MainVC.m
+//  AudioAndVideo
+//
+//  Created by fy on 16/9/8.
+//  Copyright © 2016年 LY. All rights reserved.
+//
+
+#import "MainVC.h"
+
+//define this constant if you want to use Masonry without the 'mas_' prefix
+#define MAS_SHORTHAND
+
+//define this constant if you want to enable auto-boxing for default syntax
+#define MAS_SHORTHAND_GLOBALS
+
+#import "Masonry.h"
+
+@interface MainVC ()
+
+@end
+
+@implementation MainVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+-(void)createUpUI{
+    
+}
+
+@end
