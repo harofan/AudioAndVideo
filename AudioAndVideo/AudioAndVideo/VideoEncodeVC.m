6e112649702115fab1444d2c33d9e603736a9c79
fy <fanyang_32012@outlook.com>
2016-11-25 17:44:33 +0800

    1.添加摄像头权限 2.完成了后摄像头视频获取的渲染屏幕

--- AudioAndVideo/AudioAndVideo/VideoEncodeVC.m
+++ AudioAndVideo/AudioAndVideo/VideoEncodeVC.m
@@ -8,7 +8,9 @@
 
 #import "VideoEncodeVC.h"
 
-@interface VideoEncodeVC ()
+#import <AVFoundation/AVFoundation.h>
+
+@interface VideoEncodeVC ()<AVCaptureVideoDataOutputSampleBufferDelegate>
 
 @end
 
@@ -19,21 +21,103 @@
     // Do any additional setup after loading the view.
     
     self.view.backgroundColor = [UIColor whiteColor];
+    
+   //开始获取数据
+    [self startGetData];
+    
+    //开始编码
+    [self encode];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
+/**
+ 开始获取数据
+ */
+-(void)startGetData{
+    
+    //创建后置摄像头
+    AVCaptureDevice * avCaptureDevice = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+    
+    //打开摄像头
+    NSError * error = nil;
+    
+    //创建输入设备
+    AVCaptureDeviceInput * videoInput = [AVCaptureDeviceInput deviceInputWithDevice:avCaptureDevice error:&error];
+    
+    //创建失败
+    if (!videoInput) {
+        
+        NSLog(@"%@",error);
+        
+        return;
+    }
+    
+    //创建会话,将输入输出结合在一起,并可以开始自动捕获设备
+    AVCaptureSession *avCaptureSession = [[AVCaptureSession alloc]init];
+    
+    //设置采集质量
+    avCaptureSession.sessionPreset = AVCaptureSessionPresetHigh;//默认就为高
+    
+    //添加输入流
+    [avCaptureSession addInput:videoInput];
+    
+    //采集数据从视频
+    AVCaptureVideoDataOutput * avCaptureVideoDataOutput = [[AVCaptureVideoDataOutput alloc]init];
+    
+    //设置参数
+    NSDictionary * settings = @{(__bridge id)kCVPixelBufferPixelFormatTypeKey:@(kCVPixelFormatType_420YpCbCr8BiPlanarVideoRange)};
+    
+    avCaptureVideoDataOutput.videoSettings = settings;
+    
+    //创建队列
+    dispatch_queue_t queue = dispatch_queue_create("myQueue", NULL);
+    
+    //设置代理和队列
+    [avCaptureVideoDataOutput setSampleBufferDelegate:self queue:queue];
+    
+    //会话添加输出
+    [avCaptureSession addOutput:avCaptureVideoDataOutput];
+    
+    //添加预览界面
+    AVCaptureVideoPreviewLayer * previewLayer = [AVCaptureVideoPreviewLayer layerWithSession:avCaptureSession];
+    
+    previewLayer.frame = self.view.frame;
+    
+    //保留纵横比
+    previewLayer.videoGravity = AVVideoScalingModeResizeAspectFill;
+    
+    [self.view.layer addSublayer:previewLayer];
+    
+    [avCaptureSession startRunning];
+
 }
 
-/*
-#pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+
+/**
+ 编码
+ */
+-(void)encode{
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
-*/
 
+#pragma mark -  delegate
+static int i = 0;
+-(void)captureOutput:(AVCaptureOutput *)captureOutput didOutputSampleBuffer:(CMSampleBufferRef)sampleBuffer fromConnection:(AVCaptureConnection *)connection{
+
+}
 @end
